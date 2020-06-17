ad19fab66bfa0c966b257934408ac4e563b5c443
Fatimaalbahar <albaharfatema18@gmail.com>
2020-06-17 22:33:16 +0300

    Classwork

---
+++ ios-cw2-p1.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+let family = ["Fatima","Waleed","Fahad"]
+
+var evens = [2,4,6,8,10,12]
+
+var grades = [90.5,93.2,99.3 ]
+
+var trueFalse = [true, true, true, false]
+
+print (family[0], family[2])
---
+++ ios-cw2-p2.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var fruits = [ "mango", "orange", "potato", "peach"]
+
+print(fruits[0])
+print(fruits[3])
+
+fruits.append ("strawberry")
+fruits.remove(at: 2)
+
+fruits += ["banana", "watermelon", "pineapple", "kiwi"]
+
+fruits.removeAll()
+print(fruits)
+ 
